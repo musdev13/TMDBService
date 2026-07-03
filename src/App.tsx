86c4270af0372b44537ae64aa0a47fdef0d54569
MusDev13 <sonics0977703996@gmail.com>
2026-07-03 18:32:03 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return <>
+    <div className="container mx-auto py-8 px-8">
+      <h1 className="font-bold text-center text-blue-600">TMDB Service</h1>
+    </div>
+  </>
+}
+
+export default App

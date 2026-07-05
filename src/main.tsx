f66b3c4776598d7ccd89921af9a4aeda080d8e81
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-07-05 19:21:08 +0300

    react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { About } from "./pages/About.tsx";
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/movie",
+    element: (
+      <div className="container mx-auto">
+        <h1 className="text-green-600 text-5xl text-center">Movie</h1>
+      </div>
+    ),
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
-)
+);
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-center text-red-400 text-5xl font-bold">About</h1>
+    </div>
+  );
+};

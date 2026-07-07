ebaab28c5c6eba5a37106061a6a42e76781ca598
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-07-07 11:59:55 +0300

    save

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,30 +1,33 @@
-import type { Movie } from "../App";
+import { Link } from "react-router";
+import type { Movie } from "../pages/Home";
 interface MovieCardProps {
-    movie: Movie;
+  movie: Movie;
 }
 
 export default function MovieCard({ movie }: MovieCardProps) {
-    const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+  const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
 
-    return (
-        <div className="border grid grid-cols-2 rounded-lg p-4 shadow-sm hover:shadow-md transition">
-            {movie.poster_path ? (
-                <img
-                    src={`${imageBaseUrl}${movie.poster_path}`}
-                    alt={movie.title}
-                    className="rounded-md w-full h-[250px] object-cover"
-                />
-            ) : (
-                <div className="rounded-md w-full h-[250px] bg-gray-200 flex items-center justify-center text-gray-400 text-sm">
-                    Немає фото
-                </div>
-            )}
-            <div className="pl-4">
-                <h2 className="text-lg font-bold">{movie.title}</h2>
-                <p className="text-sm text-gray-600 line-clamp-5 mt-2">
-                    {movie.overview || "Опис відсутній."}
-                </p>
-            </div>
+  return (
+    <Link to={`/movie/${movie.id}/1`}>
+      <div className="border grid grid-cols-2 rounded-lg p-4 shadow-sm hover:shadow-md transition">
+        {movie.poster_path ? (
+          <img
+            src={`${imageBaseUrl}${movie.poster_path}`}
+            alt={movie.title}
+            className="rounded-md w-full h-[250px] object-cover"
+          />
+        ) : (
+          <div className="rounded-md w-full h-[250px] bg-gray-200 flex items-center justify-center text-gray-400 text-sm">
+            Немає фото
+          </div>
+        )}
+        <div className="pl-4">
+          <h2 className="text-lg font-bold">{movie.title}</h2>
+          <p className="text-sm text-gray-600 line-clamp-5 mt-2">
+            {movie.overview || "Опис відсутній."}
+          </p>
         </div>
-    );
+      </div>
+    </Link>
+  );
 }
